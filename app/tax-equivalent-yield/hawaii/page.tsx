534f06ec3cee722caf9813f5157d3edc96763514
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:56:37 -0800

    Delete app/tax-equivalent-yield/hawaii directory

--- app/tax-equivalent-yield/hawaii/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['HI'].metadata;
-
-export default function HawaiiTEYPage() {
-  return <StatePageTemplate stateCode="HI" content={STATE_CONTENT['HI']} />;
-}
