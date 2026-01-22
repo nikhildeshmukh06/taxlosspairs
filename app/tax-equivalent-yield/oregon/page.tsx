9709cc47a7b67d5257c1b43a79fa9e7d5682bf05
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:55:51 -0800

    Delete app/tax-equivalent-yield/oregon directory

--- app/tax-equivalent-yield/oregon/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['OR'].metadata;
-
-export default function OregonTEYPage() {
-  return <StatePageTemplate stateCode="OR" content={STATE_CONTENT['OR']} />;
-}
