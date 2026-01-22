aa404b5fd6a61f69955ffdf57dbf40e46c0f722f
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:56:31 -0800

    Delete app/tax-equivalent-yield/massachusetts directory

--- app/tax-equivalent-yield/massachusetts/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['MA'].metadata;
-
-export default function MassachusettsTEYPage() {
-  return <StatePageTemplate stateCode="MA" content={STATE_CONTENT['MA']} />;
-}
