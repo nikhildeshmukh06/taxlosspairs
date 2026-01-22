cd909e5423a57b5b3351f294580c5264c064cebf
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:56:00 -0800

    Delete app/tax-equivalent-yield/new-york directory

--- app/tax-equivalent-yield/new-york/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['NY'].metadata;
-
-export default function NewYorkTEYPage() {
-  return <StatePageTemplate stateCode="NY" content={STATE_CONTENT['NY']} />;
-}
