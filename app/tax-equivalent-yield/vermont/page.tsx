a25c89efcb07f73d587705cd708ce67b7e47db0c
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:55:36 -0800

    Delete app/tax-equivalent-yield/vermont directory

--- app/tax-equivalent-yield/vermont/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['VT'].metadata;
-
-export default function VermontTEYPage() {
-  return <StatePageTemplate stateCode="VT" content={STATE_CONTENT['VT']} />;
-}
