8a80789727055586dcfe95a3e2cffdd906c2439d
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:56:45 -0800

    Delete app/tax-equivalent-yield/dc directory

--- app/tax-equivalent-yield/dc/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['DC'].metadata;
-
-export default function DCTEYPage() {
-  return <StatePageTemplate stateCode="DC" content={STATE_CONTENT['DC']} />;
-}
