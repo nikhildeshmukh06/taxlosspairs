0117a43b46b9a43530becf8e367f2fc708a579d7
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:57:27 -0800

    Delete app/tax-equivalent-yield/california directory

--- app/tax-equivalent-yield/california/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['CA'].metadata;
-
-export default function CaliforniaTEYPage() {
-  return <StatePageTemplate stateCode="CA" content={STATE_CONTENT['CA']} />;
-}
