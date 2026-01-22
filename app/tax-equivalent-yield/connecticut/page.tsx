c1dee5f5e169d2c0b3d5b272dcb28fb97bdfaecb
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:57:20 -0800

    Delete app/tax-equivalent-yield/connecticut directory

--- app/tax-equivalent-yield/connecticut/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['CT'].metadata;
-
-export default function ConnecticutTEYPage() {
-  return <StatePageTemplate stateCode="CT" content={STATE_CONTENT['CT']} />;
-}
