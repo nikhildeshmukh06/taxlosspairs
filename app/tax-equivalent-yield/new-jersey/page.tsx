9ca7d4c0ab55c47bb40cfd149e0c5e01b70a4e70
nikhildeshmukh06 <deshmukh.nikhil@gmail.com>
2026-01-22 11:56:09 -0800

    Delete app/tax-equivalent-yield/new-jersey directory

--- app/tax-equivalent-yield/new-jersey/page.tsx
+++
@@ -1,8 +0,0 @@
-import { StatePageTemplate } from '../../components/StatePageTemplate';
-import { STATE_CONTENT } from '../../data/state-content';
-
-export const metadata = STATE_CONTENT['NJ'].metadata;
-
-export default function NewJerseyTEYPage() {
-  return <StatePageTemplate stateCode="NJ" content={STATE_CONTENT['NJ']} />;
-}
